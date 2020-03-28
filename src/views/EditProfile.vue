4400ec3f60314d115d97b0e8412b70ec74815d08
jiepijiang <1069841961@qq.com>
2020-03-28 16:29:15 +0800

    个人编辑页全部完成

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -11,9 +11,30 @@
     </div>
     <!-- 组价的调用,单双标签都可以 -->
     <!-- <Listbar v-for="(item,index) in rows" :key="index" :label="item.label" :tips="item.tips" /> -->
-    <Listbar label="昵称" :tips="userInfo.nickname" />
-    <Listbar label="密码" tips="******" />
-    <Listbar label="性别" :tips="['女','男'][userInfo.gender]" />
+    <!-- 编辑昵称的按钮 -->
+    <Listbar label="昵称" :tips="userInfo.nickname" @click.native="show=true" />
+    <!-- 编辑昵称的弹窗 -->
+    <!-- v-modle:value 和input/change 事件两种数据的集合，这里的v-model作用是控制弹窗是否显示出来 -->
+    <van-dialog v-model="show" title="修改昵称" show-cancel-button @confirm="handleChangeNickname">
+      <van-field v-model="nickname" placeholder="请输入昵称" />
+    </van-dialog>
+
+    <!-- 编辑密码的按钮 -->
+    <Listbar label="密码" tips="******" @click.native="showpwd=true" />
+    <!-- 编辑密码的弹窗 -->
+    <van-dialog v-model="showpwd" title="修改密码" show-cancel-button @confirm="handleChangePassword">
+      <van-field v-model="password" placeholder="请输入新密码" type="password" />
+    </van-dialog>
+
+    <!-- 编辑性别的按钮 -->
+    <Listbar label="性别" :tips="['女','男'][userInfo.gender]" @click.native="showGender=true" />
+    <!-- 编辑性别的上拉菜单弹窗 -->
+    <van-action-sheet
+      v-model="showGender"
+      :actions="actions"
+      @select="onSelect"
+      close-on-click-action
+    />
   </div>
 </template>
 
@@ -27,7 +48,25 @@ export default {
     return {
       userInfo: {},
       // 本地的用户数据
-      userJson: {}
+      userJson: {},
+      // 是否显示编辑昵称的弹窗
+      show: false,
+      // 是否显示修改密码的弹窗
+      showpwd: false,
+      // 是否显示修改性别的弹窗
+      showGender: false,
+      // 性别弹窗的选项
+      actions: [
+        // 对象的数据是可以随意的添加和修改，
+        // 因为后台数据表示性别的数字1和0
+        { name: "男", value: 1 },
+        { name: "女", value: 0 }
+      ],
+      // 单独记录弹出框的昵称
+      nickname: "",
+      // 单独记录弹窗输入框的密码
+      password: "",
+      gender: ""
     };
   },
 
@@ -41,6 +80,8 @@ export default {
     const userJson = JSON.parse(localStorage.getItem("userInfo"));
     // 保存到data，就可以在methods的方法里面使用了
     this.userJson = userJson;
+    // console.log(this.userJson);
+
     // 请求用户详情
     this.$axios({
       url: "/user/" + userJson.user.id,
@@ -50,6 +91,8 @@ export default {
     }).then(res => {
       const { data } = res.data;
       this.userInfo = data;
+      this.nickname = this.userInfo.nickname;
+      // console.log(this.userInfo);
     });
   },
 
@@ -95,8 +138,30 @@ export default {
         data
       }).then(res => {
         console.log(res);
-        this.$toast.success("头像修改好了");
+        this.$toast.success("修改成功");
       });
+    },
+
+    // 修改昵称的事件
+    handleChangeNickname() {
+      // 调用编辑用户信息的函数
+      this.handleEdit({ nickname: this.nickname });
+      // 同步修改当前显示的数据
+      this.userInfo.nickname = this.nickname;
+    },
+
+    // 修改密码的事件
+    handleChangePassword() {
+      this.handleEdit({ password: this.password });
+      // this.userInfo.password = this.password;
+    },
+
+    // 选中性别的时候触发的时间，item是选择的当前项
+    onSelect(item) {
+      // 默认情况下点击选项时不会自动收起
+      // 可以通过 close-on-click-action 属性开启自动收起
+      this.handleEdit({ gender: item.value });
+      this.userInfo.gender = item.value;
     }
   }
 };
